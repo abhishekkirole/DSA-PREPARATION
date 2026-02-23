31f85870f5fce3fb53d469eb13344d2f76fa8d2b
abhishek <162462241+abhishekkirole@users.noreply.github.com>
2026-02-23 13:53:34 +0530

    Implement array sorted check in Solution class

--- 01-Arrays/	 Check if the Array is Sorted II/solution.java
+++ 01-Arrays/	 Check if the Array is Sorted II/solution.java
@@ -1,1 +1,22 @@
+class Solution {
+    // Function to check if the array is sorted
+    public boolean isSorted(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1])  // If any element is smaller than the previous one, return false
+                return false;
+        }
+        return true;  // Return true if the array is sorted
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        Solution obj = new Solution();
+        
+        // Output result
+        System.out.println(obj.isSorted(arr, n) ? "True" : "False");
+    }
+}
 
